a84e76be63b164db5aa736ca58607c9cc1474b6d
James Mah <junyeumah@gmail.com>
2016-10-16 17:45:36 +1100

    updated javascript file

--- scripts/memotile.js
+++ scripts/memotile.js
@@ -1,24 +1,24 @@
-var game = {start: false,
-            turnColorId: 0,
+$('html').css('cursor', 'url(./images/mouse-red.png), auto'); // Nice cursor!
+var timeOuts = []; // Keep track of the timeOuts used for effects
+
+ // Game information
+var game = {firstRound: true, // First time chosing a grid size
+            start: false, // Has the current round started
+            turnColorId: 0, // Current player turn
             otherColorId: 1,
-            pStartColorId: 0,
+            pStartColorId: 0, // Player who started the round
             pNextColorId: 1
             };
 
-function randTurnStart() {
-  var firstToStart = Math.floor(Math.random() * 2);
-  game.turnColorId = firstToStart;
-  game.otherColorId = (firstToStart + 1) % 2;
-  game.pStartColorId = firstToStart;
-  game.pNextColorId = (firstToStart + 1) % 2;
-}
+// Grid information
+var grid = {array: [], // 1D array
+            opened: [], // 1D array
+            size: 0, // Length of 1D array
+            length: 0 // Length of rows/cols
+            };
 
+// Initialise players            
 var players = [new newPlayer('red'), new newPlayer('blue')];
-$.each(players, function(i, player) {
-  $('#characters-' + player.color + ' > img:eq(' + player.character + ')')
-    .toggleClass('scale-160 hover-scale-160');
-});
-
 function newPlayer(color) {
   this.color = color;
   this.character = 1; // Initial pokemon
@@ -27,47 +27,38 @@ function newPlayer(color) {
   this.tilesLeft = 0;
 }
 
-
-var grid = {array: [], // ij array of tiles
-            opened: [], // using tileNumbers
-            length: 0, // length
-            size: 0, // total tiles
-            };
-
-
-function initTilesMax(players, gridSize, pOther) {
-  $.each(players, function(i, player) {
-    player.tilesMax = Math.floor(gridSize/2);
-    player.tilesLeft = player.tilesMax;
-  });
-  players[pOther].tilesMax += gridSize % 2;
-  players[pOther].tilesLeft = players[pOther].tilesMax;
-}
+// Update the size of the initial pokemon in the menu
+$.each(players, function(i, player) {
+  $('#characters-' + player.color + ' > img:eq(' + player.character + ')')
+    .toggleClass('scale-160 hover-scale-160');
+});
 
 
-$('html').css('cursor', 'url(images/mouse-' + players[game.turnColorId].color + '.png), auto');
+$('.characters').hover(function() {
+  var colorId = $('.characters').index($(event.target).closest('.characters'));
+  $('html').css('cursor', 'url(./images/mouse-' + players[colorId].color + '.png), auto');
+}, function() {
+  if (!game.firstRound) {
+    $('html').css('cursor', 'url(./images/mouse-' + players[game.turnColorId].color + '.png), auto');
+  }
+});
 
+// Grid image hover effect
+$('.gridsize').hover(function() {
+  $(this).find('img').fadeIn(200);
+}, function() {
+  $(this).find('img').fadeOut(200);
+});
 
-
-function newGame() {
-  $('#display-win').fadeOut();
-  initTilesMax(players, grid.size, game.pStartColorId);
-  grid.array = initArray(grid.size, game.pStartColorId, game.pNextColorId, players[game.otherColorId].tilesMax);
-  grid.opened = [];
-
-  $('#player-start').text(players[game.pStartColorId].color.toUpperCase() + ' START');
-  $('#player-start').css('color',players[game.pStartColorId].color);
-  $('#display-win').fadeIn();
-  
-}
-
+// Generate and display board after clicking on start screen
 $('#display-win').click(function() {
   game.start = true;
+  $('#display-win').fadeOut(); // Hide start screen
   placeGridTiles(grid.array, grid.length, players);
-  displayTilesLeft(players, game.turnColorId, game.otherColorId);
-  $('#display-grid > div > img, .tilesleft').hide();
-  $('#display-win').fadeOut();
-  $('#display-grid > div > img, .tilesleft').fadeIn();
+  $('.tilesleft > img').remove();
+  displayTilesLeft(players[0]);
+  displayTilesLeft(players[1]);
+  $('#display-grid > div > img, .tilesleft').hide().fadeIn();
   setTimeout(function() {
     $('#display-grid > div > img').fadeOut();
   }, grid.length * grid.length * grid.length * 20 + 100);
@@ -77,82 +68,147 @@ $('#display-win').click(function() {
   }, grid.length * grid.length * grid.length * 20 + 700);
 });
 
+// Open and close menu
+$('#button-menu').click(function() {
+  toggleDisplayMenu();
+});
 
+$('.characters img').click(function () {
+  var colorId = $('.characters').index($(event.target).closest('.characters'));
+  var tileNumber = $('#characters-' + players[colorId].color + ' > img').index($(event.target));
+
+  if (tileNumber !== players[colorId].character) {
+    $('#characters-' + players[colorId].color + ' > img:eq(' + tileNumber + ')')
+      .toggleClass('scale-160 hover-scale-160');
+    $('#characters-' + players[colorId].color + ' > img:eq(' + players[colorId].character + ')')
+      .toggleClass('scale-160 hover-scale-160');
+    players[colorId].character = tileNumber;
+    updateCharImg(colorId);
+  }
+});
+
+$('#button-score').click(function() {
+  displayScore(0, 0);
+  displayScore(1, 0);
+});
+
+$('.gridsize').click(function() {
+  $('.tilesleft > img').fadeOut();
+  randTurnStart();
+  $('html').css('cursor', 'url(./images/mouse-' + players[game.turnColorId].color + '.png), auto');
+  var gridId = $('.gridsize').index($(event.target).closest('.gridsize'));
+  grid.length = gridId + 3;
+  grid.size = (gridId + 3) * (gridId + 3);
+  toggleDisplayMenu();
+  $('aside#left *').fadeIn();
+  $('#player-wins').text('');
+  placeGridTiles(grid.array, grid.length, null);
+  newGame();
+  game.firstRound = false;
+  game.start = false;
+});
+
+// Place a random starting player for a new game mode
+function randTurnStart() {
+  var firstToStart = Math.floor(Math.random() * 2);
+  game.turnColorId = firstToStart;
+  game.otherColorId = (firstToStart + 1) % 2;
+  game.pStartColorId = firstToStart;
+  game.pNextColorId = (firstToStart + 1) % 2;
+}
+
+// Update the player details after initialising grid size
+function initTilesMax() {
+  $.each(players, function(i, player) {
+    player.tilesMax = Math.floor(grid.size/2);
+    player.tilesLeft = player.tilesMax;
+  });
+  players[game.pStartColorId].tilesMax += grid.size % 2;
+  players[game.pStartColorId].tilesLeft = players[game.pStartColorId].tilesMax;
+}
+
+// Initialise new game, show starting screen
+function newGame() {
+  $('#display-win').fadeOut();
+  initTilesMax();
+  grid.array = initArray(game.pStartColorId, game.pNextColorId, players[game.otherColorId].tilesMax);
+  grid.opened = [];
+  $('#player-start').text(players[game.pStartColorId].color.toUpperCase() + ' START');
+  $('#player-start').css('color',players[game.pStartColorId].color);
+  $('#display-win').fadeIn();
+}
 
 function initGridButtons() {
   $('#display-grid > div').click(function() {
-    var tileNumber = $( '#display-grid > div' ).index($(event.target).closest('div'));
-    $('#display-grid > div:eq(' + tileNumber + ')').removeClass('hover-red hover-blue');
-    $('#display-grid > div:eq(' + tileNumber + ') > img').addClass('hopping');
-    $('#display-grid > div > img:eq(' + tileNumber + ')').fadeIn();
-
+    var tileNumber = $( '#display-grid > div' ).index($(this));
+    var colorId = grid.array[tileNumber];
+    var player = players[colorId];
     if (grid.opened[tileNumber] !== true) {
+      $(this).removeClass('hover-red hover-blue');
+      $(this).find('img').fadeIn().addClass('hopping');
       grid.opened[tileNumber] = true;
-      $('#tilesleft-' + players[grid.array[tileNumber]].color + ' > img:eq(' + (players[grid.array[tileNumber]].tilesLeft - 1) + ')').fadeOut();
-      if ( --players[grid.array[tileNumber]].tilesLeft === 0) {
-        players[grid.array[tileNumber]].score++;
-        $('#player-wins').text(players[grid.array[tileNumber]].color.toUpperCase() + ' WINS!');
-        $('#player-wins').css('color',players[grid.array[tileNumber]].color);
-        displayScore(grid.array[tileNumber], players[grid.array[tileNumber]].score);
-        switchTurn(game.pNextColorId);
-        game.pNextColorId = game.pStartColorId;
-        game.pStartColorId = (game.pStartColorId + 1) % 2;
-        newGame();
+      $('#tilesleft-' + player.color + ' > img:eq(' + (player.tilesLeft - 1) + ')').fadeOut();
+      // Player wins if there are no tiles left
+      if ( --player.tilesLeft === 0) {
+        winRound(player, colorId);
       }
-      else if (grid.array[tileNumber] !== game.turnColorId) {
+      // Switch player if opened the wrong grid
+      else if (colorId !== game.turnColorId) {
+        switchTurn(game.otherColorId);
         displaySwitchturn();
       }
     }
   });
 }
 
+function winRound(player, colorId) {
+  player.score++;
+  displayScore(colorId, player.score);
+  $('#player-wins').text(player.color.toUpperCase() + ' WINS!')
+                   .css('color', player.color);
+  //Update next round player starting turns
+  game.pNextColorId = game.pStartColorId;
+  game.pStartColorId = (game.pStartColorId + 1) % 2;
+  switchTurn(game.pStartColorId);
+  newGame();
+}
 
-// switch to input colorId
+// Switch turn to input colorId
 function switchTurn(colorId) {
   game.turnColorId = colorId;
   game.otherColorId = (colorId + 1) % 2;
   $('.hover-red, .hover-blue').toggleClass('hover-red hover-blue');
-  $('html').css('cursor', 'url(images/mouse-' + players[game.turnColorId].color + '.png), auto');
+  $('html').css('cursor', 'url(./images/mouse-' + players[game.turnColorId].color + '.png), auto');
 }
 
 function displaySwitchturn() {
-  switchTurn(game.otherColorId);
-  $('#player-turn').text(players[game.turnColorId].color.toUpperCase() + '\'S TURN');
-  $('#player-turn').css('color',players[game.turnColorId].color);
+  var playerColor = players[game.turnColorId].color;
+  $('#player-turn').text(playerColor.toUpperCase() + '\'S TURN')
+                   .css('color', playerColor);
   $('#display-switchturn').fadeIn();
   setTimeout(function() {
     $('#display-switchturn').fadeOut();
   },1000);
 }
 
-
-function displayTilesLeft(players, pTurn, pOther) {
-  $('.tilesleft > img').remove();
-  
-  for (var i = 0; i < players[0].tilesLeft; i++) {
+// Display right aside section
+function displayTilesLeft(player) {
+  for (var i = 0; i < player.tilesLeft; i++) {
     var $newImg = $('<img>');
-    $newImg.attr("src","images/" + players[0].color + players[0].character + ".svg");
-    $('#tilesleft-' + players[0].color).append($newImg);
+    $newImg.attr("src","./images/" + player.color + player.character + ".svg");
+    $('#tilesleft-' + player.color).append($newImg);
   }
-
-  for (var j = 0; j < players[1].tilesLeft; j++) {
-    var $newImg = $('<img>');
-    $newImg.attr("src","images/" + players[1].color + players[1].character + ".svg");
-    $('#tilesleft-' + players[1].color).append($newImg);
-  }
-
 }
 
-
-function initArray(gridSize, pStart, pOther, pOtherMax) {
+// Initialise array with random placement of player characters
+function initArray(pStart, pOther, pOtherMax) {
   var array = [];
-  for (var i = 0; i < gridSize; i++) {
+  for (var i = 0; i < grid.size; i++) {
     array[i] = pStart;
   }
   var pOtherTileCount = 0;
   var tempRand;
   while (pOtherTileCount < pOtherMax) {
-    console.log(pOtherTileCount);
     tempRand = Math.floor(Math.random() * grid.size);
     if (array[tempRand] !== pOther) {
       array[tempRand] = pOther;
@@ -162,38 +218,20 @@ function initArray(gridSize, pStart, pOther, pOtherMax) {
   return array;
 }
 
-
-$('.gridsize').hover(function() {
-  $(this).find('img').fadeIn(200);
-}, function() {
-  $(this).find('img').fadeOut(200);
-});
-
-$('#button-menu').click(function() {
-  toggleDisplayMenu();
-});
-
-
-
 function placeGridTiles(array, gridLength, players) {
   $('#display-grid > div').remove();
-
   for(var i = 0; i < grid.size; i++){
     var $newTileDiv = $('<div><img></div>');
-
     if (players !== null) {
-      $newTileDiv.find('img').attr("src","images/" + players[array[i]].color + players[array[i]].character + ".svg");
+      $newTileDiv.find('img').attr("src","./images/" + players[array[i]].color + players[array[i]].character + ".svg");
     }
-    // $newTileDiv.addClass('hover-red');
-    // $newTileDiv.find('img').addClass('hopping');
     $('#display-grid').append($newTileDiv);
   }
   $('#display-grid > div').width('calc(' + (100 - (gridLength * 2) ) + '% / ' + gridLength + ')');
   $('#display-grid > div').height('calc(' + (100 - (gridLength * 2) ) + '% / ' + gridLength + ')');
 }
 
-
-
+// Open and closing of menu, and rotation of menu button
 function toggleDisplayMenu() {
   if ( $('#display-menu').css('display') === 'none') {
     clearTimeOuts();
@@ -211,8 +249,6 @@ function toggleDisplayMenu() {
   }
 }
 
-var timeOuts = [];
-
 function delayClass($element, className, duration) {
   timeOuts.push(setTimeout(function() {
     $element.attr('class', className);
@@ -226,14 +262,7 @@ function clearTimeOuts() {
   timeOuts = [];
 }
 
-
-
-// ASIDE LEFT // ASIDE LEFT // ASIDE LEFT
-$('#button-score').click(function() {
-  displayScore(0, 0);
-  displayScore(1, 0);
-});
-
+// Update scoreboard of a single color
 function displayScore(colorId, score) {
   var scoreDIV = $('.score:eq(' + colorId + ')');
   scoreDIV.slideUp(500);
@@ -242,49 +271,6 @@ function displayScore(colorId, score) {
     scoreDIV.text(score).slideDown(500);
   },500);
 }
-
-
-// DISPLAY MENU // DISPLAY MENU // DISPLAY MENU 
-$('.characters img').click(function () {
-  var colorId = $('.characters').index($(event.target).closest('.characters'));
-  var tileNumber = $('#characters-' + players[colorId].color + ' > img').index($(event.target));
-
-  if (tileNumber !== players[colorId].character) {
-    $('#characters-' + players[colorId].color + ' > img:eq(' + tileNumber + ')')
-      .toggleClass('scale-160 hover-scale-160');
-    $('#characters-' + players[colorId].color + ' > img:eq(' + players[colorId].character + ')')
-      .toggleClass('scale-160 hover-scale-160');
-    players[colorId].character = tileNumber;
-    updateCharImg(colorId);
-    // console.log(colorId);
-  }
-});
-
-
-$('.characters').hover(function() {
-  var colorId = $('.characters').index($(event.target).closest('.characters'));
-  $('html').css('cursor', 'url(images/mouse-' + players[colorId].color + '.png), auto');
-}, function() {
-  if (game.start) {
-    $('html').css('cursor', 'url(images/mouse-' + players[game.turnColorId].color + '.png), auto');
-  }
-});
-
-
-$('.gridsize').click(function() {
-  randTurnStart();
-  $('html').css('cursor', 'url(images/mouse-' + players[game.turnColorId].color + '.png), auto');
-
-  // console.log(event.target);
-  var gridId = $('.gridsize').index($(event.target).closest('.gridsize'));
-  grid.length = gridId + 3;
-  grid.size = (gridId + 3) * (gridId + 3);
-  toggleDisplayMenu();
-  $('aside *').fadeIn();
-  $('#player-wins').text('');
-  placeGridTiles(grid.array, grid.length, null);
-  newGame();
-});
 
 //update to new charImg without effects
 function updateCharImg(colorId) {
@@ -296,9 +282,8 @@ function updateCharImg(colorId) {
     // Update display grid images
     for(var i = 0; i < grid.size; i++){
       if ( grid.array[i] === colorId) {
-        $( '#display-grid img:eq(' + i + ')' ).attr("src","images/" + players[colorId].color + players[colorId].character + ".svg");
+        $( '#display-grid img:eq(' + i + ')' ).attr("src","./images/" + players[colorId].color + players[colorId].character + ".svg");
       }
     }
   }
 }
-
